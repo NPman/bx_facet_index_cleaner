9f9113ebec7d2f3a7f20b174747e4f4b1dda170a
npman <sadpn7@gmail.com>
2017-01-09 17:01:00 +0500

    Добавлен скрипт отчистки фасетного индекса для удаленных инфоблоков

---
+++ clean.php
@@ -0,0 +1,50 @@
+<?
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+
+use Bitrix\Main\Application;
+
+$connection = Application::getInstance()->getConnection('default');
+
+CModule::IncludeModule('perfmon');
+CModule::IncludeModule('iblock');
+
+$tables = CPerfomanceTableList::GetList(false);
+
+$iblocksWithIndex = [];
+
+$re = '/(b_iblock_(\d{3,4})_index$)/';
+
+while($table = $tables->Fetch()) {
+
+    preg_match($re, $table['TABLE_NAME'], $matches);
+
+    if($matches[2] > 0) {
+
+        $iblockId = $matches[2];
+
+        $iblocksWithIndex[] = $iblockId;
+    }
+}
+
+sort($iblocksWithIndex);
+
+$iblocks = CIBlock::GetList([], ['ID' => $iblocksWithIndex]);
+
+$iblocksExists = [];
+
+while($iblock = $iblocks->Fetch()) {
+
+    $iblocksExists[] = $iblock['ID'];
+}
+
+$clearIblocksIndex = array_diff($iblocksWithIndex, $iblocksExists);
+
+$iblocks = $clearIblocksIndex;
+
+foreach($iblocks as $iblockId) {
+
+    Bitrix\Iblock\PropertyIndex\Manager::DeleteIndex($iblockId);
+    Bitrix\Iblock\PropertyIndex\Manager::markAsInvalid($iblockId);
+}
+?>
